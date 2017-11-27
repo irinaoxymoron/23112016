03afc92831a112e914f8e29414a5ac8cee589a73
irinaoxymoron <irinaoxymoron@gmail.com>
2017-11-27 15:21:48 +0300

    Learn JS

---
+++ README.md
@@ -0,0 +1,11 @@
+# Base JS, MDN
+
+## developer.mozilla.org
+
+## Getting started:
+
+* git clone this repo
+* cd path/to/
+* npm install gulpjs/gulp#4.0 --save-dev
+* npm install gulp-sass gulp-sourcemaps gulp-clean-css gulp-if gulp-plumber autoprefixer gulp-watch  gulp-uglify gulp-file-include browser-sync --save-dev
+* run "gulp" command to start, work and fun
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCSS = require('gulp-clean-css'),
+    gulpif = require('gulp-if'),
+    concat = require('plumber-concat'),
+    notify = require('gulp-notify'),
+    plumber = require('gulp-plumber'),
+    autoprefixer = require('autoprefixer'),
+    watch = require('gulp-watch'),
+    uglify = require('gulp-uglify'),
+    fileinclude = require('gulp-file-include'),
+    browserSync = require('browser-sync').create();
+
+var app_dir = 'app';
+var wp_dir = 'dist';
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: wp_dir
+        }
+    });
+});
+
+gulp.task('sass', function () {
+    return gulp.src(app_dir + '/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(wp_dir + '/css'));
+});
+
+gulp.task('moveJs', function () {
+    return gulp.src([
+        app_dir + '/js/main.js'
+    ])
+        //.pipe(plumber({ errorHandler: onError }))
+        //.pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(wp_dir + '/js'));
+});
+
+gulp.task('moveHtml', function () {
+    return gulp.src(app_dir + '/*.html')
+        .pipe(fileinclude())
+        .pipe(gulp.dest(wp_dir));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(app_dir + '/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch(app_dir + '/js/*.js', gulp.series('moveJs'));
+    gulp.watch(app_dir + '/**/*.html', gulp.series('moveHtml'));
+});
+
+gulp.task('rebase', gulp.series(
+    'moveHtml',
+    'moveJs',
+    'sass'
+));
+
+gulp.task('default', gulp.series('rebase', gulp.parallel('browser-sync', 'watch')));
